cb2ca9c04daa7dfd9bf68f9bc6ee831477cb44a8
Walison Vinicios <walison.vinicios12@gmail.com>
2023-05-05 00:43:56 -0300

    Adicionando funcionalidade de criar, excluir e listar cofres

--- project/vaults/urls.py
+++ project/vaults/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('client/vault/', views.vault, name='vault'),
+    path('client/create_vault/', views.create_vault, name='create_vault'),
+    path('client/delete_vault/<int:vault_id>/', views.delete_vault, name='delete_vault'),
 ]
--- project/vaults/views.py
+++ project/vaults/views.py
@@ -1,9 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
+
+from .models import Vault
 
 def home(request):
   return render(request, 'home/index.html')
 
 @login_required
 def vault(request):
-  return render(request, 'vault/index.html')
+  user = request.user.id
+  vaults = Vault.objects.filter(user_id=user)
+  return render(request, 'vault/index.html', {'vaults': vaults})
+
+@login_required
+def create_vault(request):
+
+  if request.method == 'POST':
+      name = request.POST.get('name')
+      description = request.POST.get('description')
+      user_id = request.user.id
+
+      exemplo = Vault(name=name, description=description, user_id=user_id)
+      exemplo.save()
+      
+      return redirect('vault')
+  else:
+      return redirect('vault')
+
+def delete_vault(request, vault_id):
+  vault = Vault.objects.get(id=vault_id)
+  vault.delete()
+  return redirect('vault')
