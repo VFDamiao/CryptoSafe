cf09d92e987924ac10087f453cf32cf59bbae364
Walison Vinicios <walison.vinicios12@gmail.com>
2023-05-16 22:24:00 -0300

    Começando o desenvolvimento das credenciais (CRUD simples)

--- project/vaults/urls.py
+++ project/vaults/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path('client/edit_vault/<int:vault_id>/', views.edit_vault, name='edit_vault'),
     path('client/update_vault/<int:vault_id>/', views.update_vault, name='update_vault'),
     path('client/my-profile/', views.my_profile, name='my_profile'),
+    path('client/credential/<int:vault_id>/', views.credential, name='credential'),
+    path('client/create_credential/<int:vault_id>/', views.create_credential, name='create_credential'),
+    path('client/delete_credential/<int:credential_id>/', views.delete_credential, name='delete_credential'),
+    path('client/update_credential/<int:credential_id>/', views.update_credential, name='update_credential'),
 ]
--- project/vaults/views.py
+++ project/vaults/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
 
-from .models import Vault
+from .models import Vault, Credential
 
 def home(request):
   return render(request, 'home/index.html')
@@ -56,3 +56,47 @@ def my_profile(request):
   user = request.user.id
 
   return render(request, 'vault/my_profile.html', {'user': user})
+
+
+def credential(request, vault_id):
+  credentials = Credential.objects.filter(vault_id=vault_id)
+  vault = Vault.objects.get(id=vault_id)
+  user = request.user.id
+
+  return render(request, 'vault/credential.html', {'vault': vault, 'credentials': credentials, 'user':user})
+
+def create_credential(request, vault_id):
+  if request.method == 'POST':
+    name = request.POST.get('name')
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    description = request.POST.get('description')
+    user = request.user.id
+    vault = vault_id
+
+    credential = Credential(name=name, username=username, password=password, description=description, user_id=user, vault_id=vault)
+    credential.save()
+    
+    return redirect('credential', vault_id)
+  else:
+    return redirect('credential', vault_id)
+  
+def delete_credential(resquest, credential_id):
+  credential = Credential.objects.get(id=credential_id)
+  vault_id = credential.vault_id
+  credential.delete()
+
+  return redirect('credential', vault_id)
+
+def update_credential(request, credential_id):
+  credential = get_object_or_404(Credential, id=credential_id)
+
+  if request.method == 'POST':
+    credential.name = request.POST.get('name')
+    credential.username = request.POST.get('username')
+    credential.password = request.POST.get('password')
+    credential.description = request.POST.get('description')
+    credential.save()
+    return redirect('credential', credential.vault_id)
+  else:
+    return redirect('credential', credential.vault_id)
